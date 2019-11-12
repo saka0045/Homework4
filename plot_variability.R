8a8657d647e3e94a389d75d213ca7e8bfd2e27e4
m006703 <sakai.yuta@mayo.edu>
2019-11-11 21:28:07 -0600

    added Rscript to plot the variability

---
+++ plot_variability.R
@@ -0,0 +1,17 @@
+# The script needs to be called with:
+# Rscript plot_variability.R /path/to/variability.csv
+args <- commandArgs(trailingOnly=TRUE)
+
+# Read in the variability csv file
+cat("Reading in:\n")
+print(args[1])
+variability_table <- read.csv(args[1], header = TRUE)
+
+# Create PDF and plot
+pdf(file = "variability.pdf", width = 16, height = 8)
+plot(variability_table$Base, variability_table$Smoothed.Variability, type = "l", 
+     xlab = "Position (bp)", ylab = "Sequence Identity (%)", 
+     main = "Sequence Identity (%) vs Position (bp)")
+
+cat("\nScript is done running\n")
+cat("Variability graph is saved as variability.pdf\n")
